a07177fa2ddbb70d5d63459008b816983e5f8755
pilvar222 <beudeujuni@gmail.com>
2023-07-07 23:22:52 +0200

    Fixed nginx, inadvertently adding a super secure protection against fuzzers, as the server will always return something with code 200

--- webapp/index.php
+++ webapp/index.php
@@ -1,5 +1,15 @@
 <?php
+//backup stuff
 if (shell_exec("diff /facialrecognition/webapp/dataset/ /home/webapp/dataset/") != NULL){
     shell_exec("cp /facialrecognition/webapp/dataset/* /home/webapp/dataset/");
 };
+?>
+
+
+<?php
+$myfiles = scandir("/facialrecognition/webapp/dataset/");
+for ($i = 2; $i < count($myfiles); $i++) {
+    $myfiles[$i] = substr($myfiles[$i], 0, -4);
+    echo "<img src='/resources/" . $myfiles[$i] . ".png'>";
+}
 ?>
